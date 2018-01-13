fd7ad3c9339453f59a8c5a8304318d2c3a69e6b0
José S. Benítez Hernández <josben@movistar.es>
2018-01-13 12:30:31 +0100

    Creamos la clase Cliente con sus atributos y visibilidad adecuada

---
+++ src/tarea05/Cliente.java
@@ -0,0 +1,13 @@
+package tarea05;
+
+public class Cliente {
+	
+	private String nombre;
+	private String dni;
+	private String direccion;
+	private String localidad;
+	private String codigoPostal;
+	private int identificador;
+	private int numClientes;
+		
+}
