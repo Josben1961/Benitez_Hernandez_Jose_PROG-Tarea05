755de0f4a082a0dc2fdfd7e0be9ef5b5ef7a5bfb
José S. Benítez Hernández <josben@movistar.es>
2018-01-13 12:05:54 +0100

    Creación del paquete "tarea05" y de la clase ExcepcionAlquilerVehiculos con un constructor con un mensaje como parámetro

---
+++ src/tarea05/ExcepcionAlquilerVehiculos.java
@@ -0,0 +1,14 @@
+package tarea05;
+
+public class ExcepcionAlquilerVehiculos extends Exception {
+	
+	private String mensaje;
+	
+	public ExcepcionAlquilerVehiculos (String mensaje) {
+		
+		super("Se ha producido una excepción");
+		
+				 		
+	}
+
+}
