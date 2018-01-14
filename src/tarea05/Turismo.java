a84d7f7522b48dfd7667d8bdeb9d598fcd3956f7
José S. Benítez Hernández <josben@movistar.es>
2018-01-14 12:43:16 +0100

    Creamos constructor con cuatro parámetros, método privado compruebeMatrícula, método constructor copia y métodos getter y setter de la clase Turismo.

--- src/tarea05/Turismo.java
+++ src/tarea05/Turismo.java
@@ -1,5 +1,82 @@
 package tarea05;
 
-public class Turismo {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import utilidades.Entrada;
 
+public class Turismo {
+	
+	private String matricula;
+	private String marca;
+	private String modelo;
+	private int cilindrada;
+	private boolean disponible;
+	
+	// Objeto de la clase Turismo
+	Turismo turismo=new Turismo(matricula, marca, modelo, cilindrada);
+	
+	// Constructor con cuatro parámetros
+	public Turismo(String matricula, String marca, String modelo, int cilindrada) {
+		this.matricula=matricula;
+		this.marca=marca;
+		this.modelo=modelo;
+		this.cilindrada=cilindrada;
+
+	}
+	
+	// Constructor copia de la clase Turismo
+		public Turismo(Turismo turismo) {
+			this.matricula=turismo.getMatricula();
+			this.marca=turismo.getMarca();
+			this.modelo=turismo.getModelo();
+			this.cilindrada=turismo.getCilindrada();
+
+		}
+	
+	// Métodos getter
+	public String getMatricula() {
+		return matricula;
+	}
+
+
+	public String getMarca() {
+		return marca;
+	}
+
+
+	public String getModelo() {
+		return modelo;
+	}
+
+
+	public int getCilindrada() {
+		return cilindrada;
+	}
+
+
+	public boolean isDisponible() {
+		return disponible;
+	}
+
+	// Método setter para disponible
+	public void setDisponible(boolean disponible) {
+		this.disponible = disponible;
+	}
+
+
+	
+	// Método para comprobar la matrícula introducida.
+	// Matrícula correcta: Cuatro números, espacio y tres letras mayúsculas
+	// (excluyendo vocales)
+	private void compruebaMatricula() throws ExcepcionAlquilerVehiculos {
+		Pattern patron = Pattern.compile("([0-9]{4})+ ([BCDFGHJKLMNPQRSTVWXYZ]{3})");
+		Matcher emparejador;
+
+		emparejador = patron.matcher(matricula);
+		// Lanzamos excepción si la matrícula es erronea
+		if (!emparejador.matches()) {
+			throw new ExcepcionAlquilerVehiculos("El formato de la matrícula es erroneo");
+		}
+
+	}
 }
