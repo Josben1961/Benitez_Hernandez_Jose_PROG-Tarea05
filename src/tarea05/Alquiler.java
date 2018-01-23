cb524f560bca52961a78684da867817540bee708
José S. Benítez Hernández <josben@movistar.es>
2018-01-23 10:18:47 +0100

    Creación de los métodos getter

--- src/tarea05/Alquiler.java
+++ src/tarea05/Alquiler.java
@@ -11,7 +11,7 @@ public class Alquiler {
 	private int dias;	
 	private final SimpleDateFormat FORMATO_FECHA = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 	private final int MS_DIA = 1000 * 60 * 60 * 24;
-	private final double PRECIO_DIA = 30 * dias + (turismo.getCilindrada()/100);
+	private final double PRECIO_DIA = 30;
 	
 	
 	// Constructor con dos parámetros
@@ -22,8 +22,25 @@ public class Alquiler {
 		dias = 0;
 		turismo.setDisponible(false);
 	}
-	
-	
-	
 
+	// Métodos getters
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+
+	public Turismo getTurismo() {
+		return turismo;
+	}
+
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+
+	public int getDias() {
+		return dias;
+	}
+			
 }
