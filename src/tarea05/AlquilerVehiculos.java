96379cbeb39a1e594a593a61b3cbe64dd79030c7
José S. Benítez Hernández <josben@movistar.es>
2018-01-27 09:47:48 +0100

    Creamos Método get de Cliente con un parámetro (dni) para saber si existe este cliente.

--- src/tarea05/AlquilerVehiculos.java
+++ src/tarea05/AlquilerVehiculos.java
@@ -8,13 +8,14 @@ public class AlquilerVehiculos {
 	private Turismo[] turismos;
 	private Cliente[] clientes;
 	private Alquiler[] alquileres;
-	
+
 	// Constructor por defecto
 	public AlquilerVehiculos() {
 		turismos = new Turismo[MAX_TURISMOS];
 		clientes = new Cliente[MAX_CLIENTES];
 		alquileres = new Alquiler[MAX_ALQUILERES];
 	}
+
 	// Métodos getters
 	public Turismo[] getTurismos() {
 		return turismos;
@@ -27,5 +28,23 @@ public class AlquilerVehiculos {
 	public Alquiler[] getAlquileres() {
 		return alquileres;
 	}
+
+	// Método get de Cliente con un parámetro (dni) para saber si existe este
+	// cliente
+	public Cliente getCliente (String dni) {
+		int posicion = 0;
+		boolean clienteDni = false;
+		for (int i = 0; i < clientes.length; i++) {
+			if(clientes[i].getDni().equals(dni)) {
+				clienteDni = true;
+				posicion = i;
+			}
+		}
+		if(clienteDni) {
+			return clientes[posicion];
+		}else {
+			return null;
+		}
 	
+	}
 }
